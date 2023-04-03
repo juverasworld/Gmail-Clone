5981dfed14eb8429034e2337e83003cf3535d42d
juverasworld <108337782+juverasworld@users.noreply.github.com>
2023-04-03 06:31:10 -0600

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react"
+
+export default function MyApp({ Component, pageProps }){
+
+  return (
+    <ChakraProvider>
+     
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout"
+export default function Home(){
+  return(
+    <section>
+      <Layout>
+        Gmail
+      </Layout>
+    </section>
+  )
+}
